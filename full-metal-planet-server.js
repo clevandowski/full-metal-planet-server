a525f8957e1ee1912a266b8d6a6c68c5bfb61402
Cyrille Levandowski <cyrille.levandowski@zenika.com>
2015-05-07 01:19:26 +0200

    Le serveur est maintenant opérationnel ! Les fichiers case.js, engine.js, fmp-constants.js, partie.js, plateau.js, referee.js, tools.js sont maintenant partagés entre l'application cliente angular.js et le server node.js. Penser à synchroniser à la main ces fichiers le temps que je trouve une solution élégante :)

--- full-metal-planet-server.js
+++ full-metal-planet-server.js
@@ -2,64 +2,80 @@ var http = require('http');
 var url = require('url');
 var queryString = require('querystring');
 
-var fmpConstants = require('fmp-constants');
-var referee = require('referee');
-var player = require('player');
-var piece = require('piece');
+var fmpConstants = require('fmp-constants').fmpConstants;
+// var referee = require('referee');
+var Player = require('player').Player;
+var Piece = require('piece').Piece;
+var plateau = require('plateau').getPlateau();
+var tools = require('tools').tools;
+var fmpCaseService = require('case').fmpCaseService;
+var Partie = require('partie').Partie;
+var Referee = require('referee').Referee;
+var Engine = require('engine').Engine;
+var partie = null;
+var referee = null;
+var engine = null;
 
 var ProcessAction = function(playerAction) {
-	var actionReport;
-	// console.log('Input playerAction: ' + JSON.stringify(playerAction));
-	switch (playerAction.actionType.value) {
-		case fmpConstants.PLAYER_ACTION_TYPE.MOVE.value:
-			JSON.stringify(referee.validateMove(playerAction));
-			actionReport = {
-				success: false,
-				errorMessages: ['Déplacement pas encore implémenté côté serveur']
-			}
-			break;
-		case fmpConstants.PLAYER_ACTION_TYPE.LOAD.value:
-			// return referee.validateLoad(playerAction);
-			actionReport = {
-				success: false,
-				errorMessages: ['Chargement pas encore implémenté côté serveur']
-			}
-			break;
-		case fmpConstants.PLAYER_ACTION_TYPE.UNLOAD.value:
-			// return referee.validateUnload(playerAction);
-			actionReport = {
-				success: false,
-				errorMessages: ['Déchargement pas encore implémenté côté serveur']
-			}
-			break;
-		case fmpConstants.PLAYER_ACTION_TYPE.ATTACK.value:
-			// return referee.validateAttack(playerAction);
-			actionReport = {
-				success: false,
-				errorMessages: ['Attaque pas encore implémentée côté serveur']
-			}
-			break;
-		case fmpConstants.PLAYER_ACTION_TYPE.END_OF_TURN.value:
-			actionReport =  { success: true };
-			break;
-		default:
-			console.log('Unknow playerAction: ' + JSON.stringify(playerAction));
-			actionReport = {
-				success: false, 
-				errorMessages: ['Unknow playerActionType: ' + playerAction.actionType.value]
-			}
-			break;
+	if (partie == null) {
+		console.log('ERREUR: Impossible de traiter les actions tant qu\'une partie n\'est pas créée. http://localhost:8080/init');
+		return null;
+	}
+	console.log('current player: ' + JSON.stringify(partie.getPlayer()));
+
+	var actionReport = referee.validatePlayerAction(playerAction, null);
+	if (actionReport.success) {
+		engine.applyPlayerAction(playerAction);
 	}
 	return actionReport;
 }
 
-var ProcessResponse = function(actionReport, response) {
+var ProcessPartieInitialisation = function() {
+	var randomCurrentMaree = Math.floor((Math.random() * 3));
+	var randomNextMaree = Math.floor((Math.random() * 3));
+	var datas = {
+		tour: 0,
+		tourPlayer: 0,
+		tourPoints: 15,
+		currentMaree: fmpConstants.MAREES[randomCurrentMaree],
+		nextMaree: fmpConstants.MAREES[randomNextMaree],
+		players: [
+			// C'est très important que l'id des joueurs suive l'ordre de 
+			// l'index du tableau car on les indexe par l'id ensuite
+			// TODO N'utiliser que l'id comme identifiant
+			new Player(0, 'Damien'),
+			new Player(1, 'Noémie')
+		],
+		pieces: [
+			new Piece(1, fmpConstants.PIECE_TYPE.TANK, 2, 9), 
+			new Piece(1, fmpConstants.PIECE_TYPE.TANK, 3, 9), 
+			// new Piece(2, 1, PIECE_TYPE.TANK, 2, 8), 
+			new Piece(0, fmpConstants.PIECE_TYPE.TANK, 5, 9), 
+			new Piece(0, fmpConstants.PIECE_TYPE.TANK, 6, 9), 
+			new Piece(0, fmpConstants.PIECE_TYPE.TANK, 6, 8), 
+			new Piece(0, fmpConstants.PIECE_TYPE.TANK, 7, 8),
+			new Piece(0, fmpConstants.PIECE_TYPE.TANK, 7, 7),
+			new Piece(1, fmpConstants.PIECE_TYPE.TANK, 33, 11),
+			new Piece(1, fmpConstants.PIECE_TYPE.TANK, 34, 12), 
+			new Piece(1, fmpConstants.PIECE_TYPE.TANK, 34, 13),
+			new Piece(0, fmpConstants.PIECE_TYPE.BARGE, 7, 9, fmpConstants.ORIENTATION.SO),
+			new Piece(1, fmpConstants.PIECE_TYPE.BARGE, 33, 12, fmpConstants.ORIENTATION.SO)
+		]
+	};
+	partie = new Partie(fmpConstants, datas, plateau, tools, fmpCaseService);
+	referee = new Referee(fmpConstants, partie, tools);
+    engine = new Engine(fmpConstants, partie, tools);
+	console.log('partie: ' + JSON.stringify(partie.getPlayer()));
+	return datas;
+}
+
+var ProcessResponse = function(object, response) {
 	response.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
-	if (actionReport != null) {
-		console.log('actionReport: ' + JSON.stringify(actionReport));
-		response.write(JSON.stringify(actionReport));
+	if (object != null) {
+		console.log('response: ' + JSON.stringify(object));
+		response.write(JSON.stringify(object));
 	} else {
-		console.log('No action report to send...');
+		console.log('No response to send...');
 	}
   	response.end();
 }
@@ -74,38 +90,8 @@ var RequestListener = function(request, response) {
 			console.log(JSON.stringify(data));
 		});
 		request.on('end', function () {
-			var randomCurrentMaree = Math.floor((Math.random() * 3));
-			var randomNextMaree = Math.floor((Math.random() * 3));
-			var datas = {
-				tour: 0,
-				tourPlayer: 0,
-				tourPoints: 15,
-				currentMaree: fmpConstants.MAREES[randomCurrentMaree],
-				nextMaree: fmpConstants.MAREES[randomNextMaree],
-				players: [
-					// C'est très important que l'id des joueurs suive l'ordre de 
-					// l'index du tableau car on les indexe par l'id ensuite
-					// TODO N'utiliser que l'id comme identifiant
-					new player.Player(0, 'Damien'),
-					new player.Player(1, 'Noémie')
-				],
-				pieces: [
-					new piece.Piece(1, fmpConstants.PIECE_TYPE.TANK, 2, 9), 
-					new piece.Piece(1, fmpConstants.PIECE_TYPE.TANK, 3, 9), 
-					// new Piece(2, 1, PIECE_TYPE.TANK, 2, 8), 
-					new piece.Piece(0, fmpConstants.PIECE_TYPE.TANK, 5, 9), 
-					new piece.Piece(0, fmpConstants.PIECE_TYPE.TANK, 6, 9), 
-					new piece.Piece(0, fmpConstants.PIECE_TYPE.TANK, 6, 8), 
-					new piece.Piece(0, fmpConstants.PIECE_TYPE.TANK, 7, 8),
-					new piece.Piece(0, fmpConstants.PIECE_TYPE.TANK, 7, 7),
-					new piece.Piece(1, fmpConstants.PIECE_TYPE.TANK, 33, 11),
-					new piece.Piece(1, fmpConstants.PIECE_TYPE.TANK, 34, 12), 
-					new piece.Piece(1, fmpConstants.PIECE_TYPE.TANK, 34, 13),
-					new piece.Piece(0, fmpConstants.PIECE_TYPE.BARGE, 7, 9, fmpConstants.ORIENTATION.SO),
-					new piece.Piece(1, fmpConstants.PIECE_TYPE.BARGE, 33, 12, fmpConstants.ORIENTATION.SO)
-				]
-			};
-			// console.log('Partie datas: ' + JSON.stringify(datas));
+			var datas = ProcessPartieInitialisation();
+			console.log('partie datas: ' + JSON.stringify(datas));
 			ProcessResponse(datas, response);
 			return;
 		});
@@ -134,6 +120,10 @@ var RequestListener = function(request, response) {
 	  	response.end();
 	  	return;
 	}
+}
+
+var initPartie = function() {
+	
 }
 
 var server = http.createServer(RequestListener);
